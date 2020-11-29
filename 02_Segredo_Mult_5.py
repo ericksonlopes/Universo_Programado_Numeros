35ef5592c75c6aa8e7cebd6a3c13dcc8d5036597
Erickson-lopes-dev <ofc.erickson@gmail.com>
2020-11-29 11:37:37 -0300

    organized 02

--- 02_Segredo_Mult_5.py
+++ 02_Segredo_Mult_5.py
@@ -2,7 +2,7 @@ for numero in range(1, 10):
     lista_cinco = [5, 10, 15, 20, 25, 30, 35, 40, 45]
 
     # criar uma função para exibir
-    print(f'{"Número da vez " + str(numero - 5):^25} ')
+    print(f'{"Número da vez " + f"{numero}, utiliza a tabuada do: " + str(numero - 5):^25} ')
     for c in range(1, 10):
         num_opera = 0
         if numero < 5:
@@ -11,6 +11,6 @@ for numero in range(1, 10):
         elif numero > 5:
             num_opera = '+ ' + str((numero - 5) * c)
 
-        print(f'  {numero} x {c} = {numero * c:<3} '
-              f' {lista_cinco[c - 1]:>2} {num_opera:^3} = {numero * c:<2}')
+        print(f'      {numero} x {c} = {numero * c:<3} '
+              f' {lista_cinco[c - 1]:>3} {num_opera:^4} = {numero * c:<3}')
     print()
