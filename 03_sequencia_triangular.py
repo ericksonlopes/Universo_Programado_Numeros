7b08914580dc9c670864913c64dc60f1de80db85
Erickson-lopes-dev <ofc.erickson@gmail.com>
2020-12-01 11:32:07 -0300

    03 finalized

--- 03_sequencia_triangular.py
+++ 03_sequencia_triangular.py
@@ -8,10 +8,3 @@ for num in range(1, 15):
 
 print('\n ', lista)
 
-# for num in range(1, 15):
-#     lista_cont = [n for n in range(1, num)]
-#     print(f'{num:^3}', f'{str(lista_cont):^47}')
-#
-#     lista.append(sum(lista_cont) )
-#
-# print('\n ', lista)
