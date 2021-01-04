9ee0c8e838b8253ccdce4ef0aa7edbe99a27aad1
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-01-03 23:36:30 -0300

    mult 9

---
+++  Matemática_&_CIA/mul_6_pares.py
@@ -0,0 +1,3 @@
+[print(f'6 x {num:>2} = {num * 6}') for num in range(1, 50) if num % 2 == 0]
+
+# todos os numeros multiplcados por 6 seu ultimo é igual ao do resultado
---
+++  Matemática_&_CIA/mult_9_macete.py
@@ -0,0 +1,13 @@
+[print(f'9 x {num:>2} = {9 * num:>3}  ({num - len(str(num))}{((num - len(str(num))) - 9 * len(str(num))) * -1})')
+ for num in range(1, 20)]
+
+# a cada casa decimal a sequencia muda
+
+# 9 x 4(uma casa)
+# 4 - 1 = 3
+# 3 (4, 5, 6, 7, 8, 9) = 6 até 9 apartir do 3
+# 36
+
+# 9 x 14(duas casas)
+# 14 - 2 = 12
+# 12 (13, 14, 15, 16, 17, 18) 6 até 18 apartir do 12
